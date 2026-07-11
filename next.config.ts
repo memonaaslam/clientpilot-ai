c597f74c94455ca378ffb3a02d83fed31d513e55
memonaaslam <memonaaslam00@gmail.com>
2026-07-11 21:19:16 +0500

    Change ClientPilot base path to clientpilotai

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/copilotai"
+  basePath: "/clientpilotai"
 };
 
 export default nextConfig;
